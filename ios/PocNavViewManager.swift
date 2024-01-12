0ed85554c5a2e3fe7ef503226a1603ee371f7e32
uffoltzl <l.ufz@hotmail.fr>
2024-01-12 18:30:20 +0100

    --wip-- [skip ci]

--- ios/PocNavViewManager.swift
+++ ios/PocNavViewManager.swift
@@ -1,36 +1,12 @@
+import MapboxNavigation
+
 @objc(PocNavViewManager)
 class PocNavViewManager: RCTViewManager {
-
-  override func view() -> (PocNavView) {
-    return PocNavView()
+  override func view() -> (NavigationMapView) {
+    return NavigationMapView()
   }
 
   @objc override static func requiresMainQueueSetup() -> Bool {
     return false
-  }
-}
-
-class PocNavView : UIView {
-
-  @objc var color: String = "" {
-    didSet {
-      self.backgroundColor = hexStringToUIColor(hexColor: color)
-    }
-  }
-
-  func hexStringToUIColor(hexColor: String) -> UIColor {
-    let stringScanner = Scanner(string: hexColor)
-
-    if(hexColor.hasPrefix("#")) {
-      stringScanner.scanLocation = 1
-    }
-    var color: UInt32 = 0
-    stringScanner.scanHexInt32(&color)
-
-    let r = CGFloat(Int(color >> 16) & 0x000000FF)
-    let g = CGFloat(Int(color >> 8) & 0x000000FF)
-    let b = CGFloat(Int(color) & 0x000000FF)
-
-    return UIColor(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: 1)
   }
 }
--- react-native-poc-nav.podspec
+++ react-native-poc-nav.podspec
@@ -15,6 +15,8 @@ Pod::Spec.new do |s|
   s.source       = { :git => "https://github.com/uffoltzl/react-native-poc-nav.git", :tag => "#{s.version}" }
 
   s.source_files = "ios/**/*.{h,m,mm,swift}"
+  
+  s.dependency "MapboxNavigation", "~> 2.17"
 
   # Use install_modules_dependencies helper to install the dependencies if React Native version >=0.71.0.
   # See https://github.com/facebook/react-native/blob/febf6b7f33fdb4904669f99d795eba4c0f95d7bf/scripts/cocoapods/new_architecture.rb#L79.
@@ -37,6 +39,6 @@ Pod::Spec.new do |s|
     s.dependency "RCTRequired"
     s.dependency "RCTTypeSafety"
     s.dependency "ReactCommon/turbomodule/core"
-   end
+  end
   end    
 end
